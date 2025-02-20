1b3fb7af10326516001e1d7461edbbac0f91c012
chinhpxhhe171177 <chinhpxhe171177@fpt.edu.vn>
2025-02-20 20:40:53 +0700

    new update

---
+++ client/src/constants/path.ts
@@ -0,0 +1,19 @@
+const path = {
+  home: '/home',
+  login: '/auth/login',
+  profile: '/user/profile',
+  changePassword: '/user/changePassword',
+  register: '/auth/register',
+  googleLogin: '/login/oauth',
+  chat: '/user/chat',
+  verifyEmail: '/user/verify-email',
+  forgotPassword: '/auth/forgot-password',
+  verifyForgotPassword: '/auth/verify-forgot-password',
+  resetPassword: '/auth/reset-password',
+  user: '/user',
+  any: '*',
+  no: '',
+  asHome: '/',
+  auth: '/auth'
+} as const
+export default path
